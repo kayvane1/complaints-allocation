9935562e0063ff3dbb9f13aa2696883ad7491cf6
kayvane <kayvane.shakerifar@gmail.com>
2022-02-15 16:02:33 +0000

    enable pytest and ci cd testing'

--- tests/unit/test_model_lambda.py
+++ tests/unit/test_model_lambda.py
@@ -1,12 +1,11 @@
 from http import HTTPStatus
-from model_lambdas.model_inference import handler
+from lambdas.inference.model_inference import handler
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 def generate_context() -> LambdaContext:
     context = LambdaContext()
     context._aws_request_id = '888888'
     return context
-
 
 def test_handler_200_ok():
     response = handler({"model_id": "Kayvane/distilbert-complaints-product", "text": "Coinbase stole all of my money" }, generate_context())
