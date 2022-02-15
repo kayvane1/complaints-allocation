f487769b316ed7a66caf3e950d224f4970633313
kayvane <kayvane.shakerifar@gmail.com>
2022-02-15 13:54:29 +0000

    lambda hardening, testing'
    git push

---
+++ model_lambdas/model_inference.py
@@ -0,0 +1,38 @@
+from http import HTTPStatus
+from typing import Any, Dict
+from transformers import pipeline
+from aws_lambda_powertools.logging.logger import Logger
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+
+def load_model(model_id: str) -> pipeline:
+    """Load a model from a model name
+
+    Args:
+        model_name (str): The name of the model to load
+
+    Returns:
+        model (transformers.pipeline.Pipeline): The loaded model
+    """
+    return pipeline(
+        task="text-classification",
+        model=model_id,
+    )
+
+
+# JSON output format, service name can be set by environment variable "POWERTOOLS_SERVICE_NAME"
+logger: Logger = Logger(service='model-inference-lambda')
+
+
+def handler(event: Dict[str, Any], context: LambdaContext) -> Dict[str, Any]:
+
+    logger.set_correlation_id(context.aws_request_id)
+    logger.debug(
+        f"{event['model_id']} model being used by lambda function for inference")
+    pipe = load_model(event['model_id'])
+    event['prediction'] = pipe(event['text'])[0]
+    
+    return {'statusCode': HTTPStatus.OK,
+            'headers': {'Content-Type': 'application/json'},
+            'body': event
+            }
---
+++ tests/unit/test_model_lambda.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+from model_lambdas.model_inference import handler
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+def generate_context() -> LambdaContext:
+    context = LambdaContext()
+    context._aws_request_id = '888888'
+    return context
+
+
+def test_handler_200_ok():
+    response = handler({"model_id": "Kayvane/distilbert-complaints-product", "text": "Coinbase stole all of my money" }, generate_context())
+    assert response['statusCode'] == HTTPStatus.OK
